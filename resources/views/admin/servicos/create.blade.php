efaf746095ede84476e1facca6785ffc45f70c4a
Gabriel Brunetti <gabriel.brunetti1@gmail.com>
2019-10-10 23:10:35 -0300

    create.blade indexacao

--- resources/views/admin/servicos/create.blade.php
+++ resources/views/admin/servicos/create.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.appPainel')
 
 @section('breadcrumb')
-<div class="row page-titles">
-        <div class="col-md-5 col-8 align-self-center">
-            <h3 class="text-themecolor">Faça seu anúncio!</h3>
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-                <li class="breadcrumb-item"><a href="{{ url('/servicos') }}">Serviços</a></li>
-                <li class="breadcrumb-item active">Anúncio</li>
-            </ol>
-        </div>
-</div>
+    <div class="row page-titles">
+            <div class="col-md-5 col-8 align-self-center">
+                <h3 class="text-themecolor">Faça seu anúncio!</h3>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+                    <li class="breadcrumb-item"><a href="{{ url('/servicos') }}">Serviços</a></li>
+                    <li class="breadcrumb-item active">Anúncio</li>
+                </ol>
+            </div>
+    </div>
 
 @endsection
 
